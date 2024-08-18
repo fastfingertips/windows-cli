6a2c6a06c310694a6450f34db170aa0e046f6f00
FastFingertips <bugra.coskun@outlook.com>
2024-08-18 22:05:53 +0300

    refactor: move power scheme functions to power_scheme_utils.py

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
-import subprocess
+import curses
+from pages.menu import display_main_menu
 
-def get_active_scheme():
-    """
-    Burada powercfg komutu ile güncel enerji planının ismini alıyoruz.
-    """
-
-    commands = {
-        'list': ['powercfg', '/list'],
-        'query': ['powercfg', '/query', 'SCHEME_CURRENT', 'SUB_SLEEP', 'STANDBYIDLE'],
-    }
-
-    result = subprocess.run(commands['list'], capture_output=True, text=True)
-    lines = result.stdout.split('\n')
-
-    for line in lines:
-        if all(x in line for x in ['*', ':']):
-            line = line.split(': ')[1].split('(')
-            guid = line[0].strip()
-            name = line[1].split(')')[0]
-
-            context = {
-                'name': name,
-                'guid': guid,
-            }
-
-            return context
-    return None
-
-current_power_plan = get_active_scheme()
-print(current_power_plan)
+if __name__ == "__main__":
+    # start the main menu
+    curses.wrapper(display_main_menu)
---
+++ utils/power_scheme_utils.py
@@ -0,0 +1,58 @@
+import subprocess
+
+def get_power_schemes():
+    """
+    retrieves the power schemes and the active power scheme.
+    """
+    commands = {
+        'list': ['powercfg', '/list'],
+        'query': ['powercfg', '/query', 'SCHEME_CURRENT', 'SUB_SLEEP', 'STANDBYIDLE'],
+    }
+
+    # get the active power scheme
+    result = subprocess.run(commands['list'], capture_output=True, text=True)
+    lines = result.stdout.split('\n')
+
+    active_scheme = None
+    power_schemes = []
+
+    for line in lines:
+        if all(x in line for x in ['*', ':']):
+            line = line.split(': ')[1].split('(')
+            guid = line[0].strip()
+            name = line[1].split(')')[0]
+
+            active_scheme = {
+                'name': name,
+                'guid': guid,
+            }
+        elif 'Power Scheme GUID' in line:
+            line = line.split(': ')[1].split('(')
+            guid = line[0].strip()
+            name = line[1].split(')')[0]
+
+            power_schemes.append({
+                'name': name,
+                'guid': guid,
+            })
+
+    return active_scheme, power_schemes
+
+def switch_power_scheme(guid: str) -> None:
+    """
+    activates the power scheme with the given guid.
+    """
+    subprocess.run(['powercfg', '/setactive', guid])
+
+if __name__ == "__main__":
+    active_scheme, power_schemes = get_power_schemes()
+    
+    print("Active Power Scheme:")
+    if active_scheme:
+        print(f"Name: {active_scheme['name']}, GUID: {active_scheme['guid']}")
+    else:
+        print("No active power scheme found.")
+    
+    print("\nAvailable Power Schemes:")
+    for scheme in power_schemes:
+        print(f"Name: {scheme['name']}, GUID: {scheme['guid']}")
