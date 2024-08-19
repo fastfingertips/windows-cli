66b2166fa33dabc78baddd91b48f4fed34cdc07b
FastFingertips <bugra.coskun@outlook.com>
2024-08-19 15:41:22 +0300

    refactor(network): improve network menu layout and readability

--- pages/tools/network.py
+++ pages/tools/network.py
@@ -1,12 +1,18 @@
 import curses
-from utils.curses_utils import show_info
+from layout import display_layout
+from utils.curses_utils import (
+  draw_page_location,
+  draw_page_keys,
+  show_info,
+  draw_text
+) 
 from utils.network_utils import (
-  get_network_status,
-  get_local_ip,
-  get_public_ip,
   get_network_adapters_status,
+  get_network_status,
+  disable_internet,
   enable_internet,
-  disable_internet
+  get_public_ip,
+  get_local_ip
 )
 
 
@@ -24,24 +30,52 @@ def display_network_menu(stdscr):
         local_ip = get_local_ip()
         public_ip = get_public_ip()
 
-        stdscr.addstr(0, 0, "Network Configuration (q to quit, h for main menu):")
-        stdscr.addstr(2, 0, f"Network Status: {network_status}")        
-        stdscr.addstr(4, 0, f"Local IP Address: {local_ip}")
-        stdscr.addstr(5, 0, f"Public IP Address: {public_ip}")
-        stdscr.addstr(7, 0, "Network Adapters:")
+        layout = display_layout(stdscr)
+        sc_height, sc_width = stdscr.getmaxyx()
 
-        for i, adapter in enumerate(adapters):
-            name = adapter['interface_name']
-            admin_state = adapter['admin_state']
-            status = adapter['state']
-            if i == selected_adapter_index:
-                stdscr.addstr(8 + i, 0, f"> {name} ({admin_state}, {status})", curses.A_REVERSE)
-            else:
-                stdscr.addstr(8 + i, 0, f"  {name} ({admin_state}, {status})")
+        center_y = sc_height // 2
+        center_x = sc_width // 2
 
-        stdscr.addstr(8 + len(adapters) + 1, 0, "Press 'e' to enable selected adapter.")
-        stdscr.addstr(8 + len(adapters) + 2, 0, "Press 'd' to disable selected adapter.")
-        stdscr.addstr(8 + len(adapters) + 3, 0, "Use arrow keys to navigate.")
+        header_y_pos = layout['header']
+        footer_y_pos = layout['footer']
+
+        draw_page_location(stdscr, header_y_pos, "Network Configuration")
+        draw_page_keys(stdscr, footer_y_pos, [
+            "'e': Enable selected adapter",
+            "'d': Disable selected adapter",
+            "'q': Quit",
+            "'h': Go back to main menu",
+            "Use arrow keys to navigate."
+        ])
+
+        # actions
+        for adapter_no, adapter in enumerate(adapters):
+            adapter_text = f"> {adapter['interface_name']} ({adapter['admin_state']}, {adapter['state']})"
+            adapter_text_y = center_y - adapter_no - 2
+            adapter_text_x = center_x - len(adapter_text) // 2
+            draw_text(
+                stdscr,
+                adapter_text_y,
+                adapter_text_x,
+                adapter_text,
+                curses.A_NORMAL if adapter_no != selected_adapter_index else curses.A_REVERSE
+            )
+
+        # network status
+        lines = [
+            f"Network Status: {network_status}",
+            f"Local IP Address: {local_ip}",
+            f"Public IP Address: {public_ip}"
+        ]
+        for line_no, line_text in enumerate(lines):
+            line_text_y = center_y + line_no
+            line_text_x = center_x - len(line_text) // 2
+            draw_text(
+                stdscr,
+                line_text_y,
+                line_text_x,
+                line_text
+            )
 
         key = stdscr.getch()
 
