2aeee3e321e5ce9b260e371e986bfc821edf5f39
FastFingertips <bugra.coskun@outlook.com>
2024-08-23 21:58:09 +0300

    feat(network): refactor network menu and add key press handler

--- pages/tools/network.py
+++ pages/tools/network.py
@@ -1,6 +1,8 @@
 import curses
 from layout import display_layout
 from utils.curses_utils import (
+  ScreenSize,
+  get_centered_text_position,
   draw_page_location,
   draw_page_keys,
   show_info,
@@ -16,43 +18,75 @@ from utils.network_utils import (
 )
 
 
+def handle_key_press(stdscr, selected_adapter_index, adapters):
+        key = stdscr.getch()
+        if key == ord('q'):
+            return 0, 0
+        elif key == curses.KEY_UP:
+            selected_adapter_index = (selected_adapter_index - 1) % len(adapters)
+        elif key == curses.KEY_DOWN:
+            selected_adapter_index = (selected_adapter_index + 1) % len(adapters)
+        elif key == ord('e'):
+            if adapters:
+                adapter = adapters[selected_adapter_index]
+                adapter_name = adapter['interface_name']
+                if adapter['admin_state'] == "Disabled":
+                    show_info(stdscr, f"Enabling {adapter_name}...", "Network", 1)
+                    success = enable_internet(adapter_name)
+                    if success:
+                        show_info(stdscr, f"Enabled {adapter_name} successfully.", "Network", 1)
+                    else:
+                        show_info(stdscr, f"Failed to enable {adapter_name}. Administrator privileges may be required.", "Error", 2)
+                else:
+                    show_info(stdscr, f"{adapter_name} is already enabled.", "Network", .5)
+        elif key == ord('d'):
+            if adapters:
+                adapter = adapters[selected_adapter_index]
+                adapter_name = adapter['interface_name']
+                if adapter['admin_state'] == "Enabled":
+                    show_info(stdscr, f"Disabling {adapter_name}...", "Network", 1)
+                    success = disable_internet(adapter_name)
+                    if success:
+                        show_info(stdscr, f"Disabled {adapter_name} successfully.", "Network", 1)
+                    else:
+                        show_info(stdscr, f"Failed to disable {adapter_name}. Administrator privileges may be required.", "Error", 2)
+                else:
+                    show_info(stdscr, f"{adapter_name} is already disabled.", "Network", .5)
+        else:
+            show_info(stdscr, "Invalid key pressed.", "Key Error", 2)
+        return 1, selected_adapter_index
+
 def display_network_menu(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(False)
     stdscr.timeout(-1)
 
+    screen_size = ScreenSize(stdscr)
     selected_adapter_index = 0
+    
 
     while True:
-        stdscr.clear()
         adapters = get_network_adapters_status()
         network_status = get_network_status()
         local_ip = get_local_ip()
         public_ip = get_public_ip()
 
         layout = display_layout(stdscr)
-        sc_height, sc_width = stdscr.getmaxyx()
-
-        center_y = sc_height // 2
-        center_x = sc_width // 2
-
         header_y_pos = layout['header']
         footer_y_pos = layout['footer']
-
+        
         draw_page_location(stdscr, header_y_pos, "Network Configuration")
         draw_page_keys(stdscr, footer_y_pos, [
-            "'e': Enable selected adapter",
-            "'d': Disable selected adapter",
+            "'e': Enable selected adapter" if adapters[selected_adapter_index]['admin_state'] != "Enabled" else "'d': Disable selected adapter",
             "'q': Quit",
-            "'h': Go back to main menu",
             "Use arrow keys to navigate."
         ])
 
         # actions
         for adapter_no, adapter in enumerate(adapters):
             adapter_text = f"> {adapter['interface_name']} ({adapter['admin_state']}, {adapter['state']})"
-            adapter_text_y = center_y - adapter_no - 2
-            adapter_text_x = center_x - len(adapter_text) // 2
+            adapter_text_y = screen_size.center_y - adapter_no - 2
+            adapter_text_x = get_centered_text_position(adapter_text, screen_size)
             draw_text(
                 stdscr,
                 adapter_text_y,
@@ -68,8 +102,8 @@ def display_network_menu(stdscr):
             f"Public IP Address: {public_ip}"
         ]
         for line_no, line_text in enumerate(lines):
-            line_text_y = center_y + line_no
-            line_text_x = center_x - len(line_text) // 2
+            line_text_y = screen_size.center_y + line_no
+            line_text_x = get_centered_text_position(line_text, screen_size)
             draw_text(
                 stdscr,
                 line_text_y,
@@ -77,25 +111,8 @@ def display_network_menu(stdscr):
                 line_text
             )
 
-        key = stdscr.getch()
-
-        if key == ord('q'):
+        action, selected_adapter_index = handle_key_press(stdscr, selected_adapter_index, adapters)
+        if not action:
             break
-        elif key == ord('h'):
-            break
-        elif key == curses.KEY_UP:
-            selected_adapter_index = (selected_adapter_index - 1) % len(adapters)
-        elif key == curses.KEY_DOWN:
-            selected_adapter_index = (selected_adapter_index + 1) % len(adapters)
-        elif key == ord('e'):
-            if adapters:
-                adapter = adapters[selected_adapter_index]['interface_name']
-                enable_internet(adapter)
-                show_info(stdscr, f"Enabled {adapter}.")
-        elif key == ord('d'):
-            if adapters:
-                adapter = adapters[selected_adapter_index]['interface_name']
-                disable_internet(adapter)
-                show_info(stdscr, f"Disabled {adapter}.")
 
-        stdscr.refresh()
+        screen_size.refresh()
