d4e8e62a7bf11c3009e36d42e050deab3b193cce
FastFingertips <bugra.coskun@outlook.com>
2024-08-23 21:57:02 +0300

    Refactor system time display to use centered text position and screen size utility functions

--- pages/tools/system_time.py
+++ pages/tools/system_time.py
@@ -1,4 +1,5 @@
 import curses
+from utils.curses_utils import ScreenSize, get_centered_text_position
 from utils.system_time_utils import get_current_datetime
 
 
@@ -8,20 +9,20 @@ def display_system_time_menu(stdscr):
     """
     curses.curs_set(0)
     stdscr.nodelay(False)
-    stdscr.timeout(-1)
+    stdscr.timeout(100)
+
+    screen_size = ScreenSize(stdscr)
+
 
     while True:
-        stdscr.clear()
-        stdscr.addstr(0, 0, "System Time Display:")
-
         current_datetime = get_current_datetime()
-        stdscr.addstr(2, 0, f"Current System Time: {current_datetime}", curses.A_BOLD)
+
+        pos = get_centered_text_position(current_datetime, screen_size)
+        stdscr.addstr(0, pos, current_datetime)
 
         key = stdscr.getch()
-
         if key == ord('q'):
             break
-        elif key == ord('h'):
-            break
 
-        stdscr.refresh()
+        screen_size.refresh()
+        stdscr.clear()
