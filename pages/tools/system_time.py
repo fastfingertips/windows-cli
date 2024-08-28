736cbb78e17dcb4cadd103f90367faf231a07e5f
FastFingertips <bugra.coskun@outlook.com>
2024-08-28 21:50:10 +0300

    refactor: extract key press handling into separate function

--- pages/tools/system_time.py
+++ pages/tools/system_time.py
@@ -2,6 +2,10 @@ import curses
 from utils.curses_utils import ScreenSize, get_centered_text_position
 from utils.system_time_utils import get_current_datetime
 
+def handle_key_press(stdscr, key):
+    key = stdscr.getch()
+    if key == ord('q'):
+        return 0
 
 def display_system_time_menu(stdscr):
     """
@@ -20,8 +24,9 @@ def display_system_time_menu(stdscr):
         pos = get_centered_text_position(current_datetime, screen_size)
         stdscr.addstr(0, pos, current_datetime)
 
-        key = stdscr.getch()
-        if key == ord('q'):
+        key = handle_key_press
+
+        if not key:
             break
 
         screen_size.refresh()
